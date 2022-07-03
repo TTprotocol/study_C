e3f2fb1c0979c3bb73d41573a83a41325a5e0101
TTprotocol <wnsdud7941@naver.com>
2022-07-03 23:29:19 +0900

    test1

---
+++ study_C/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace study_C {
+    class Program {
+        static void Main(string[] args) {
+
+            int[,,] num;
+            num = new int[20,20,20];
+
+            
+        }
+    }
+}
